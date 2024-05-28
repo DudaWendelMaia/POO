66017698e3aed50f85212fd490c03829e7f25429
Maria Maia <113213701+DudaWendelMaia@users.noreply.github.com>
2024-05-28 15:47:18 -0300

    Delete Trabalho2/Trabalho2 directory

--- Trabalho2/Trabalho2/T2 Maria Maia/src/Categoria.java
+++
@@ -1,7 +0,0 @@
-public enum Categoria {
-        ACAO,
-        ESPORTE,
-        ESTRATEGIA,
-        SIMULACAO,
-        RPG;
-    }
--- Trabalho2/Trabalho2/T2 Maria Maia/src/Cobravel.java
+++
@@ -1,4 +0,0 @@
-public interface Cobravel {
-    public double calculaPrecoVenda();
-    public double calculaImposto();
-}
